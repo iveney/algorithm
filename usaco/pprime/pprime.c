e047903ec9a2bde59a1e9ae6590172e99abf90c9
Xiao Zigang <iveney@gmail.com>
2009-02-17 13:34:19 +0800

    Finished usaco's problem:pprime

--- usaco/pprime/pprime.c
+++ usaco/pprime/pprime.c
@@ -7,9 +7,11 @@ TASK: pprime
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#define PRINT() {if( sum>=a && sum<=b && isprime(sum) ) printf("%d\n",sum);}
 
 int isprime(int n)
 {
+	if(n<=1) return 0;
 	int i;
 	int root = (int) sqrt(n);
 	for(i=2;i<=root;i++)
@@ -18,20 +20,29 @@ int isprime(int n)
 	return 1;
 }
 
+int count(int n){
+	int c=1;
+	while(n/10>0) {
+		c++;
+		n/=10;
+	}
+	return c;
+}
+
 char buf[10];
 int d[10];
 int t[]={
 	0,
 	1,	// 1
 	10,
-	100,
-	1000,
-	10000,	// 5
-	100000,
-	1000000,
-	10000000,
-	100000000, // 9
-	1000000000
+	1E2,
+	1E3,
+	1E4,	// 5
+	1E5,
+	1E6,
+	1E7,
+	1E8,
+	1E9,   // 9
 };
 
 int main()
@@ -40,29 +51,68 @@ int main()
 	freopen("pprime.out","w",stdout);
 	int a,b;
 	scanf("%d %d",&a,&b);
-	int i;
-	int sum;
-	for(d[1]=0;d[1]<=9;d[1]++)
-	{
-		if( d[1]!=0 && d[1]%2==0 ) continue;
-		for(d[2]=0;d[2]<=9;d[2]++)
-		{
-			for(d[3]=0;d[3]<=9;d[3]++)
-			{
-				for(d[4]=0;d[4]<=9;d[4]++)
-				{
-					for(d[5]=0;d[5]<=9;d[5]++)
-					{
-						sum = d[1]*t[9]+d[2]*t[8]+d[3]*t[7]+
-							d[4]*t[6]+d[5]*t[5]+
-							d[4]*t[4]+d[3]*t[3]+
-							d[2]*t[2]+d[1]*t[1];
-						if( sum > b )
-							continue;
-						printf("%d\n",sum);
+	int i=0;
+	int sum=0;
+	int c1=count(a);
+	int c2=count(b);
+	//printf("c1,c2=%d %d\n",c1,c2);
+	for(i=c1;i<=c2;i++){// digit count
+		for(d[1]=0;d[1]<=9;d[1]++) {
+			sum = d[1];
+			if(i<2){
+				PRINT();
+			}
+			else if(i==2){
+				sum = d[1]*10+d[1];
+				PRINT();
+			}
+			else{
+				for(d[2]=0;d[2]<=9;d[2]++) {
+					sum = d[1]*100+d[2]*10+d[1];
+					if(i<4){
+						PRINT();
+					}
+					else if(i==4){
+						sum = d[1]*1000+d[2]*100+d[2]*10+d[1];
+						PRINT();
+					}
+					else{
+						for(d[3]=0;d[3]<=9;d[3]++) {
+							sum = d[1]*10000+d[2]*1000+d[3]*100+d[2]*10+d[1];
+							if(i<6){
+								PRINT();
+							}
+							else if(i==6){
+								sum = d[1]*1E5+d[2]*1E4+d[3]*1E3+
+								      d[3]*1E2+d[2]*1E1+d[1];
+								PRINT();
+							}
+							else{
+								for(d[4]=0;d[4]<=9;d[4]++) {
+									sum = d[1]*1E6+d[2]*1E5+d[3]*1E4+
+										d[4]*1E3+d[3]*1E2+d[2]*10+d[1];
+									if(i<8){
+										PRINT();
+									}
+									else if(i==8){
+										sum = d[1]*1E7+d[2]*1E6+d[3]*1E5+d[4]*1E4+
+										      d[4]*1E3+d[3]*1E2+d[2]*1E1+d[1];
+										PRINT();
+									}
+									else{
+										for(d[5]=0;d[5]<=9;d[5]++) {
+											sum = d[1]*1E8+d[2]*1E7+
+												d[3]*1E6+d[4]*1E5+
+												d[5]*1E4+d[4]*1E3+
+												d[3]*1E2+d[2]*10+d[1];
+											PRINT();
+										}
+									}
+								}
+							}
+						}
 					}
 				}
-
 			}
 		}
 	}
