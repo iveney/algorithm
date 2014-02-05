46ae6088afdf41d3119861a5de6c006a0b55a70c
Zigang Xiao <iveney@gmail.com>
2014-02-05 16:14:01 -0600

    Balanced binary tree

--- leetcode/balanced-binary-tree.cpp
+++ leetcode/balanced-binary-tree.cpp
@@ -1,16 +1,43 @@
-#include <iostream>
-using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+#include "leetcode.h"
 
 class Solution {
 public:
     bool isBalanced(TreeNode *root) {
-        
+      int height;
+      return isBalanced(root, height);
+    }
+    bool isBalanced(TreeNode *root, int &height) {
+      if (root == NULL) {
+        height = 0;
+        return true;
+      }
+
+      int lh = 0, rh = 0;
+      bool l_bal = isBalanced(root->left, lh);
+      bool r_bal = isBalanced(root->right, rh);
+      height = max(lh, rh) + 1;
+      return (l_bal && r_bal && abs(lh - rh) <= 1);
     }
 };
+
+int main(int argc, char const *argv[])
+{
+  TreeNode nodes[] = {
+    TreeNode(1), // 0
+    TreeNode(2), TreeNode(2),  // 1, 2
+    TreeNode(3), TreeNode(3),  // 3, 4
+    TreeNode(4), TreeNode(4),  // 5, 6
+  };
+  nodes[0].left = &nodes[1];
+  nodes[0].right = &nodes[2];
+  nodes[1].left = &nodes[3];
+  nodes[1].right = &nodes[5];
+  nodes[2].left = &nodes[6];
+  nodes[2].right = &nodes[4];
+
+  TreeNode *root = &nodes[0];
+  Solution sol;
+  cout << sol.isBalanced(root) << endl;
+
+  return 0;
+}
