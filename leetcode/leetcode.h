2acb4d8c15a03c15c6192e3f29d9ac50adc49847
Zigang Xiao <iveney@gmail.com>
2014-02-06 18:17:21 -0600

    Serialize/deserialize binary trees

--- leetcode/leetcode.h
+++ leetcode/leetcode.h
@@ -3,9 +3,14 @@
 
 // headers for TreeNode, ListNode, and utility functions
 
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
 #include <iostream>
 #include <functional>
 #include <vector>
+#include <string>
 using namespace std;
 
 struct TreeNode {
@@ -42,24 +47,78 @@ void postorder(TreeNode *root, function<void(TreeNode*)> func) {
   func(root);
 }
 
+void cout_tree_node(TreeNode *node) {
+  if (node == NULL) return;
+  cout << node->val << " ";
+}
+
 void preorder_cout(TreeNode *root) {
-  preorder(root, [](TreeNode *r) {
-    cout << r->val << " ";
-  });
+  preorder(root, cout_tree_node);
   cout << endl;
 }
 
 void inorder_cout(TreeNode *root) {
-  inorder(root, [](TreeNode *r) {
-    cout << r->val << " ";
-  });
+  inorder(root, cout_tree_node);
   cout << endl;
 }
 
 void postorder_cout(TreeNode *root) {
-  postorder(root, [](TreeNode *r) {
-    cout << r->val << " ";
-  });
+  postorder(root, cout_tree_node);
   cout << endl;
 }
+
+void serialize_tree_recursive(FILE* f, TreeNode *root) {
+  if (root == NULL) {
+    fprintf(f, "# ");
+  } else {
+    fprintf(f, "%d ", root->val);
+    serialize_tree_recursive(f, root->left);
+    serialize_tree_recursive(f, root->right);
+  }
+}
+
+// serialize tree to file using pre-order
+void serialize_tree(const char* filename, TreeNode *root) {
+  FILE* f = fopen(filename, "w");
+  if ( f == NULL ) {
+    fprintf(stderr, "Open file %s failed.\n", filename);
+    return;
+  }
+
+  serialize_tree_recursive(f, root);
+
+  fclose(f);
+}
+
+void deserialize_tree_recursive(ifstream& ifs, TreeNode *&node) {
+  string token;
+  // ifs >> token;
+  if ( !(ifs >> token) )
+    return;
+
+  if (token == "#") {
+    node = NULL;
+    return;
+  }
+
+  int val = stoi(token);
+  node = new TreeNode(val);
+  deserialize_tree_recursive(ifs, node->left);
+  deserialize_tree_recursive(ifs, node->right);
+}
+
+TreeNode *deserialize_tree(const char* filename) {
+  ifstream ifs(filename);
+  if ( !ifs ) {
+    cerr << "Open file " << filename << "failed.\n";
+    return NULL;
+  }
+
+  TreeNode *root = NULL;
+  deserialize_tree_recursive(ifs, root);
+  ifs.close();
+
+  return root;
+}
+
 #endif
---
+++ leetcode/serialize_binary_tree.cpp
@@ -0,0 +1,26 @@
+#include "leetcode.h"
+
+int main(int argc, char const *argv[])
+{
+  
+  TreeNode nodes[] = {
+    TreeNode(1), // 0
+    TreeNode(2), TreeNode(2),  // 1, 2
+    TreeNode(3), TreeNode(3),  // 3, 4
+    TreeNode(4), TreeNode(4),  // 5, 6
+  };
+  nodes[0].left = &nodes[1];
+  nodes[0].right = &nodes[2];
+  nodes[1].left = &nodes[3];
+  nodes[1].right = &nodes[5];
+  nodes[2].left = &nodes[6];
+  nodes[2].right = &nodes[4];
+
+  TreeNode *root = &nodes[0];
+  preorder_cout(root);
+
+  serialize_tree("tmp.txt", root);
+  TreeNode *read = deserialize_tree("tmp.txt");
+  preorder_cout(read);
+  return 0;
+}
