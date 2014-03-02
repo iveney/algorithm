30d241695221a2688081b012fd124c6fca08de0c
Zigang Xiao <iveney@gmail.com>
2014-03-02 15:38:40 -0600

    Add Recover binary tree

--- leetcode/recover-binary-search-tree.cpp
+++ leetcode/recover-binary-search-tree.cpp
@@ -4,28 +4,43 @@
 
 class Solution {
   int found;
+  TreeNode *pred;
   TreeNode *wrong[2];
 public:
   void recoverTree(TreeNode *root) {
     found = 0;
-    recoverTree(root, NULL);
+    pred = NULL;
+    traverse(root);
   }
 
-  void recoverTree(TreeNode *root, TreeNode *parent) {
-    if (root == NULL || found == 2) return;
+  // pred: predecessor
+  // pred is ready when calling this function
+  void traverse(TreeNode *root) {
+    if (root == NULL) return;
 
-    if (parent != NULL && 
-        ((parent->left == root  && root->val > parent->val) ||
-         (parent->right == root && root->val < parent->val)) ) {
-      wrong[found++] = root;
+    // in-order traversal
+    traverse(root->left);
+
+    // stop traversing when both of the swapped nodes are found
+    if (found == 2) return;
+
+    cout << "Visiting " << root->val << endl;
+    if (pred != NULL && pred->val > root->val) {
+      // observation: 
+      // 1st time when pred > root: pred is the swapped node
+      // 2nd time when pred > root: root is the swapped node
+      wrong[found] = (found == 0 ? pred : root);
+      cout << "Found wrong node " << wrong[found]->val << endl;
+      ++found;
       if (found == 2) {
         swap(wrong[0]->val, wrong[1]->val);
+        return;
       }
-    } else {
-      recoverTree(root->left, root);
-      // cout << root->val << ' ';
-      recoverTree(root->right, root);
     }
+
+    pred = root; // visit this node, thus this node becomes predecessor
+
+    traverse(root->right);
   }
 };
 
@@ -35,11 +50,11 @@ int main() {
   TreeNode *root = deserialize_tree(tree);
   // level_order_cout(root);
 
-  // swap(root->left->val, root->right->val);
+  swap(root->left->val, root->right->val);
   // swap(root->val, root->right->left->val);
   // swap(root->val, root->right->val);
   // swap(root->val, root->left->val);
-  swap(root->val, root->right->left->right->val);
+  // swap(root->val, root->right->left->right->val);
   // level_order_cout(root);
   inorder_cout(root);
 
