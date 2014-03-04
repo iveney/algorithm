bf4005f77b1b1b5156bf2241a8a572712aa91215
Zigang Xiao <iveney@gmail.com>
2014-03-03 21:34:15 -0600

    Move files into folder

--- leetcode/recover-binary-search-tree.cpp
+++ leetcode/recover-binary-search-tree.cpp
@@ -11,6 +11,9 @@ public:
     found = 0;
     pred = NULL;
     traverse(root);
+    if ( found == 1 ) {
+      swap(wrong[0]->val, wrong[1]->val);
+    }
   }
 
   // pred: predecessor
@@ -24,14 +27,20 @@ public:
     // stop traversing when both of the swapped nodes are found
     if (found == 2) return;
 
-    cout << "Visiting " << root->val << endl;
+    // cout << "Visiting " << root->val << endl;
     if (pred != NULL && pred->val > root->val) {
       // observation: 
       // 1st time when pred > root: pred is the swapped node
       // 2nd time when pred > root: root is the swapped node
-      wrong[found] = (found == 0 ? pred : root);
-      cout << "Found wrong node " << wrong[found]->val << endl;
-      ++found;
+      if (found == 0) {
+        // special case: only two nodes in the tree and they are swapped
+        // we will not find 2nd time of pred > root
+        wrong[found++] = pred;
+        wrong[found] = root;
+      } else {
+        wrong[found++] = root;
+      }
+      // cout << "Found wrong node " << wrong[found]->val << endl;
       if (found == 2) {
         swap(wrong[0]->val, wrong[1]->val);
         return;
@@ -48,14 +57,19 @@ int main() {
   Solution sol;
   string tree = "50 30 20 10 # # # 40 # # 90 70 60 # # 80 # # 100 # #";
   TreeNode *root = deserialize_tree(tree);
-  // level_order_cout(root);
 
-  swap(root->left->val, root->right->val);
+  // swap(root->left->val, root->right->val);
   // swap(root->val, root->right->left->val);
   // swap(root->val, root->right->val);
   // swap(root->val, root->left->val);
-  // swap(root->val, root->right->left->right->val);
-  // level_order_cout(root);
+  // swap(root->val, root->left->val);
+  swap(root->val, root->right->left->right->val);
+
+  // string tree = "1 # 0 # #";
+  // string tree = "0 1 # # #";
+  // string tree = "1 2 # # 0 # #";
+  // TreeNode *root = deserialize_tree(tree);
+  
   inorder_cout(root);
 
   sol.recoverTree(root);
