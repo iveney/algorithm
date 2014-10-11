333513e18bedcd436ad358f09630b0c361e310b5
Zigang Xiao <iveney@gmail.com>
2014-10-11 18:26:42 -0500

    2nd visit

--- leetcode/merge-sorted-array.cpp
+++ leetcode/merge-sorted-array.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+class SolutionV2 {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        int k = m + n - 1;
+        int i = m - 1, j = n - 1;
+        while (i >= 0 && j >= 0) {
+            if (A[i] >= B[j]) {
+                A[k--] = A[i--];
+            } else {
+                A[k--] = B[j--];
+            }
+        }
+        while (j >= 0) {
+            A[k--] = B[j--];
+        }
+    }
+};
+
 // idea is to calculate space needed then merge from back
 class Solution {
 public:
