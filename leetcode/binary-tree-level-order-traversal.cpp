c19d385097f31af88429d8c5acf7e58ddfbf4000
Zigang Xiao <iveney@gmail.com>
2014-10-16 18:37:00 -0500

    Add C++11 style solution

--- leetcode/binary-tree-level-order-traversal.cpp
+++ leetcode/binary-tree-level-order-traversal.cpp
@@ -3,6 +3,39 @@
 #include <queue>
 #include <algorithm>
 
+class SolutionCPP11 {
+public:
+  vector<vector<int> > levelOrder(TreeNode *root) {
+    vector<vector<int> > result;
+    if (root == nullptr) return result;
+    queue<TreeNode *> cur, nxt;
+    cur.push(root);
+
+    vector<int> buffer;
+    while (!cur.empty()) {
+      TreeNode *node = cur.front();
+      cur.pop();
+      buffer.push_back(node->val);
+      if (node->left) {
+        nxt.push(node->left);
+      }
+      if (node->right) {
+        nxt.push(node->right);
+      }
+      if (cur.empty()) {
+        // awesome c++11 feature: constant swap
+        swap(cur, nxt);
+
+        // awesome c++11 feature: move instead of copy
+        result.push_back(move(buffer));
+        buffer.clear();
+      }
+    }
+
+    return result;
+  }
+};
+
 class Solution {
 public:
     vector<vector<int> > levelOrder(TreeNode *root) {
