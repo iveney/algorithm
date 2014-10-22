8da4ab9d128d715a165c10a1d351e698df8d3248
Zigang Xiao <iveney@gmail.com>
2014-10-21 21:01:31 -0500

    Add Comment

--- leetcode/best-time-to-buy-and-sell-stock-iii.cpp
+++ leetcode/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,8 +1,18 @@
 // very interesting problem.
 #include "leetcode.h"
 
-// idea: compute the highest from both direction
-// then the max is find a pivoting poing in between
+// An intuitive (but wrong) thought:
+// Compute all possible buy-low-sell-high segments, and choose the highest two.
+// The problem is, a buy-low-sell-high segment may cross multiple these segments and has higher profit.
+// We can take this segment and take another one to maximize profit.
+// e.g., {1, 6, 5, 10, 7, 8, 5, 7},
+// - Using the wrong idea, we can (1, 6), (5, 10), (7, 8), (5, 7), max will be (1, 6) and (5, 10) = 10
+// - Correct solution: note that (1, 6, 5, 10) gives max profit 9 (taking 1 and 10), and we can add (5, 7)=2 to maximize profit as 11
+
+// idea: use DP to compute an array L, where L[i] denotes the max among 0..i
+// also compute and array R, where R[i] denotes the max among i..n-1
+// then the max two can be found by finding a pivot that maximize L[i-1] + R[i];
+
 class Solution {
 public:
   int maxProfit(vector<int> &prices) {
@@ -35,6 +45,39 @@ public:
   }
 };
 
+class SolutionWrong {
+public:
+  int maxProfit(vector<int> &prices) {
+    vector<int> maxtwo(2, 0);
+    int n = prices.size();
+    int b = 0, e = 0;
+    while ( b < n ) {
+        // decreasing
+      while ( b+1 < n && prices[b+1] <= prices[b] )
+        ++b;
+
+      if (b == n) break;
+
+      e = b;
+        // increasing
+      while ( e == 0 || (e+1 < n && prices[e+1] >= prices[e]) )
+        ++e;
+
+      if (e == n) break;
+
+      int val = prices[e] - prices[b];
+      if (val > maxtwo[0]) {
+        maxtwo[1] = maxtwo[0];
+        maxtwo[0] = val;
+      } else if (val > maxtwo[1]) {
+        maxtwo[1] = val;
+      }
+      b = e + 1;
+    }
+    return maxtwo[0] + maxtwo[1];
+  }
+};
+
 int main() {
   vector<vector<int > > prices {
     {5, 4, 3, 2, 1, 2},
@@ -51,8 +94,10 @@ int main() {
     {8,3,6,2,8,8,8,4,2,0,7,2,9,4,9},
   };
   Solution sol;
+  SolutionWrong sol2;
   for (auto &p : prices) {
-    cout << sol.maxProfit(p) << endl;
+    cout << "sol 1: " << sol.maxProfit(p) << endl;
+    cout << "sol 2: " << sol2.maxProfit(p) << endl;
   }
   
   return 0;
