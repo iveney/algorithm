941b3ec1b70e4802568b8319cb75be216ad9b021
Zigang Xiao <iveney@gmail.com>
2014-10-25 17:01:52 -0500

    Add some explanation

--- leetcode/container-with-most-water.cpp
+++ leetcode/container-with-most-water.cpp
@@ -1,4 +1,14 @@
+// interesting idea
 #include "leetcode.h"
+
+// Key observation: the shorter boundary decides the area.
+// The only way that we can increase the area is to make the shorter boundary taller
+// If we fix right boundary and change left, those with a smaller value must be useless
+// that gives us two directions: 
+// - move left bound to left: we need to consider all as the width is increasing
+// - move left bound to right: we only need to consider the taller boundary as the width is decreasing, so this is a better choice
+// 
+// A good visualization: https://oj.leetcode.com/discuss/11482/yet-another-way-to-see-what-happens-in-the-o-n-algorithm
 
 class Solution {
 public:
