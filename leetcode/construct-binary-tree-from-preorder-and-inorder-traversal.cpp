439ae994a13fd78cd767c1ab41079d68689e141c
Zigang Xiao <iveney@gmail.com>
2014-11-05 21:27:21 -0600

    solution v2 and comments

--- leetcode/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ leetcode/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,4 +1,34 @@
+// http://leetcode.com/2011/04/construct-binary-tree-from-inorder-and-preorder-postorder-traversal.html
+// idea is to partition into two subarrays and recursively build tree
+// given inorder:
+// [ .. L .. ] [root] [ .. R .. ]
+// and preorder
+// [root] [ .. L .. ] [ .. R .. ]
 #include "leetcode.h"
+
+class SolutionV2 {
+public:
+  unordered_map<int, int> inorder_map;
+  TreeNode *buildTreeRecursive(vector<int> &preorder, vector<int> &inorder,
+    int pb, int pe, int ib, int ie) {
+    if (pb == pe) return nullptr;
+
+    int rootval = preorder[pb];
+    int pivot = inorder_map[rootval];
+    TreeNode *root = new TreeNode(rootval);
+    int lsize = pivot - ib;
+    root->left  = buildTreeRecursive(preorder, inorder, pb + 1, pb + 1 + lsize, ib, pivot);
+    root->right = buildTreeRecursive(preorder, inorder, pb + 1 + lsize, pe, pivot+1, ie);
+    return root;
+  }
+
+  TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+    for (int i = 0; i < inorder.size(); ++i) {
+      inorder_map[inorder[i]] = i;
+    }
+    return buildTreeRecursive(preorder, inorder, 0, preorder.size(), 0, inorder.size());
+  }
+};
 
 class Solution {
 public:
